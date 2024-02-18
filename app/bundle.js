114d7d26b7ea81331d7c9691e66a23aad17d8341
cwebba1 <cwebba1@gmail.com>
2024-02-17 21:48:38 -0500

    Initial commit

---
+++ app/bundle.js
@@ -0,0 +1,17 @@
+var _console;
+// (bundle from remote urls) 
+// import Whatever from 'https://unpkg.com/whatever'
+
+// (bundle from npm)
+// import Whatever from 'whatever'
+// import $ from 'blingblingjs'
+
+// (bundle from local)
+// import whatever from './whatever.js'
+
+// postCSS processed css-4-js
+// (file as string)
+// import { default as index_css }  from './css/index.css'
+
+(_console = console) === null || _console === void 0 ? void 0 : _console.log('💀');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlcyI6WyJqcy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyAoYnVuZGxlIGZyb20gcmVtb3RlIHVybHMpIFxuLy8gaW1wb3J0IFdoYXRldmVyIGZyb20gJ2h0dHBzOi8vdW5wa2cuY29tL3doYXRldmVyJ1xuXG4vLyAoYnVuZGxlIGZyb20gbnBtKVxuLy8gaW1wb3J0IFdoYXRldmVyIGZyb20gJ3doYXRldmVyJ1xuLy8gaW1wb3J0ICQgZnJvbSAnYmxpbmdibGluZ2pzJ1xuXG4vLyAoYnVuZGxlIGZyb20gbG9jYWwpXG4vLyBpbXBvcnQgd2hhdGV2ZXIgZnJvbSAnLi93aGF0ZXZlci5qcydcblxuLy8gcG9zdENTUyBwcm9jZXNzZWQgY3NzLTQtanNcbi8vIChmaWxlIGFzIHN0cmluZylcbi8vIGltcG9ydCB7IGRlZmF1bHQgYXMgaW5kZXhfY3NzIH0gIGZyb20gJy4vY3NzL2luZGV4LmNzcydcblxuY29uc29sZT8ubG9nKCfwn5KAJykiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBLENBQUEsUUFBQSxHQUFBQSxPQUFPLE1BQVAsSUFBQSxJQUFBLFFBQUEsS0FBQSxLQUFBLENBQUEsR0FBQSxLQUFBLENBQUEsR0FBQSxRQUFBLENBQVNDLEdBQUcsQ0FBQyxJQUFJLENBQUMifQ==
